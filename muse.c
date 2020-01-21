97c4cf5e5000a8219ad5dfb9278cbd9dd74aaffc
Jeremy Boggs <nicocchilover@gmail.com>
2020-01-20 21:26:46 -0500

    refactore writes to a custom buffer type

--- muse.c
+++ muse.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -111,28 +112,55 @@ int getWindowSize(int *rows, int*cols) {
     }
 }
 
+/*** append buffer ***/
+
+struct abuf {
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+    free(ab->b);
+}
+
 /*** output ***/
 
 // Draw each row of the buffer of text being edited
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; y++) {
-        write(STDIN_FILENO, "~", 1); // Writes the ~
+        abAppend(ab, "~", 1); // Writes the ~
 
         if (y < E.screenrows - 1) {
-            write(STDOUT_FILENO, "\r\n", 2);
+            abAppend(ab, "\r\n", 2);
         }
     }
 }
 
 // Clear the screen
 void editorRefreshScreen() {
-    write(STDOUT_FILENO, "\x1b[2J", 4); // Writes an escape sequence to the terminal
-    write(STDOUT_FILENO, "\x1b[H", 3); // Reposition the cursor
-    
-    editorDrawRows();
+    struct abuf ab = ABUF_INIT;
 
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[2J", 4); // Writes an escape sequence to the terminal
+    abAppend(&ab, "\x1b[H", 3); // Reposition the cursor
+    
+    editorDrawRows(&ab);
+
+    abAppend(&ab, "\x1b[H", 3);
+
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
 }
 
 /*** input ***/
