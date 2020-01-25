f4d97064ccdf4c81b21baaab326d77d93c09df0a
Jeremy Boggs <nicocchilover@gmail.com>
2020-01-25 12:21:46 -0500

    editor draws screen full of text from file

--- muse.c
+++ muse.c
@@ -42,7 +42,7 @@ struct editorConfig {
     int screenrows; // Number of rows that we can show
     int screencols; // Number of cols that we can show
     int numrows; // Number of rows
-    erow row; // Rows
+    erow *row; // Rows
     struct termios orig_termios;
 };
 
@@ -182,6 +182,19 @@ int getWindowSize(int *rows, int*cols) {
     }
 }
 
+/*** row operations ***/
+
+void editorAppendRow(char *s, size_t len) {
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+    
+    int at = E.numrows;
+    E.row[at].size = len;
+    E.row[at].chars = malloc(len + 1);
+    memcpy(E.row[at].chars, s, len);
+    E.row[at].chars[len] = '\0';
+    E.numrows++;
+}
+
 /*** file i/o ***/
 
 void editorOpen(char *filename) {
@@ -191,16 +204,12 @@ void editorOpen(char *filename) {
     char *line = NULL;
     size_t linecap = 0;
     ssize_t linelen;
-    linelen = getline(&line, &linecap, fp);
-    if (linelen != -1) {
-        while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+    while ((linelen = getline(&line, &linecap, fp)) != -1) {
+        while (linelen > 0 && (line[linelen - 1] == '\n' || 
+                               line[linelen - 1] == '\r'))
             linelen--;
         
-        E.row.size = linelen;
-        E.row.chars = malloc(linelen + 1);
-        memcpy(E.row.chars, line, linelen);
-        E.row.chars[linelen] = '\0';
-        E.numrows = 1;
+        editorAppendRow(line, linelen);
     }
     free(line);
     fclose(fp);
@@ -255,9 +264,9 @@ void editorDrawRows(struct abuf *ab) {
                 abAppend(ab, "~", 1);
             }
         } else {
-            int len = E.row.size;
+            int len = E.row[y].size;
             if (len > E.screencols) len = E.screencols;
-            abAppend(ab, E.row.chars, len);
+            abAppend(ab, E.row[y].chars, len);
         }
 
         abAppend(ab, "\x1b[K", 3);
@@ -357,6 +366,7 @@ void initEditor() {
     E.cx = 0; // Horizontal coordinate of the cursor (the column)
     E.cy = 0; // Vertical coordinate of the cursor (the row)
     E.numrows = 0;
+    E.row = NULL;
 
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
