ef028842ff3378ae63b71633b05b3b1176d8d2c5
Jeremy Boggs <nicocchilover@gmail.com>
2020-01-25 14:02:59 -0500

    backspace and delete at end of lines

--- muse.c
+++ muse.c
@@ -259,6 +259,21 @@ void editorAppendRow(char *s, size_t len) {
     E.dirty++;
 }
 
+// Free the memory of the row
+void editorFreeRow(erow *row) {
+    free(row->render);
+    free(row->chars);
+}
+
+// Delete the row
+void editorDelRow(int at) {
+    if (at < 0 || at >= E.numrows) return;
+    editorFreeRow(&E.row[at]);
+    memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - at - 1));
+    E.numrows--;
+    E.dirty++;
+}
+
 // Insert char inside of the row
 void editorRowInsertChar(erow *row, int at, int c) {
     if (at < 0 || at > row->size) at = row->size;
@@ -266,6 +281,16 @@ void editorRowInsertChar(erow *row, int at, int c) {
     memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
     row->size++;
     row->chars[at] = c;
+    editorUpdateRow(row);
+    E.dirty++;
+}
+
+// Append a string to the end of a row
+void editorRowAppendString(erow *row, char *s, size_t len) {
+    row->chars = realloc(row->chars, row->size + len + 1);
+    memcpy(&row->chars[row->size], s, len);
+    row->size += len;
+    row->chars[row->size] = '\0';
     editorUpdateRow(row);
     E.dirty++;
 }
@@ -293,11 +318,17 @@ void editorInsertChar(int c) {
 // Delete char
 void editorDelChar() {
     if (E.cy == E.numrows) return;
+    if (E.cx == 0 && E.cy == 0) return;
 
     erow *row = &E.row[E.cy];
     if (E.cx > 0) {
         editorRowDelChar(row, E.cx - 1);
         E.cx--;
+    } else {
+        E.cx = E.row[E.cy - 1].size;
+        editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+        editorDelRow(E.cy);
+        E.cy--;
     }
 }
 
