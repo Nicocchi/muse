fb8b288a2d716f64c3b140ba215821ec47242c01
Jeremy Boggs <nicocchilover@gmail.com>
2020-01-25 14:35:35 -0500

    basic search done

--- muse.c
+++ muse.c
@@ -218,6 +218,21 @@ int editorRowCxToRx(erow *row, int cx) {
     return rx;
 }
 
+// Convert RX to CX
+int editorRowRXtoCX(erow *row, int rx) {
+    int cur_rx = 0;
+    int cx;
+    for (cx = 0; cx < row->size; cx++) {
+        if (row->chars[cx] == '\t')
+            cur_rx += (MUSE_TAB_STOP - 1) - (cur_rx % MUSE_TAB_STOP);
+        cur_rx++;
+
+        if (cur_rx > rx) return cx;
+    }
+
+    return cx;
+}
+
 // Update the row
 void editorUpdateRow(erow *row) {
     int tabs = 0;
@@ -428,6 +443,27 @@ void editorSave() {
 
     free(buf);
     editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
+}
+
+/*** find ***/
+
+void editorFind() {
+    char *query = editorPrompt("Search: %s (ESC to cancel");
+    if (query == NULL) return;
+
+    int i;
+    for (i = 0; i < E.numrows; i++) {
+        erow *row = &E.row[i];
+        char *match = strstr(row->render, query);
+        if (match) {
+            E.cy = i;
+            E.cx = editorRowRXtoCX(row, match - row->render);
+            E.rowoff = E.numrows;
+            break;
+        }
+    }
+
+    free(query);
 }
 
 /*** append buffer ***/
@@ -697,6 +733,10 @@ void editorProcessKeypress() {
             if (E.cy < E.numrows)
                 E.cx = E.row[E.cy].size;
             break;
+
+        case CTRL_KEY('f'):
+            editorFind();
+            break;
         
         case BACKSPACE:
         case CTRL_KEY('h'):
@@ -766,7 +806,7 @@ int main(int argc, char *argv[]) {
         editorOpen(argv[1]);
     }
 
-    editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
+    editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
 
     while (1) {
         editorRefreshScreen();
