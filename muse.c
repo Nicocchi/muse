3170f492ccb8f99c2729bf9438300aebed885179
Jeremy Boggs <nicocchilover@gmail.com>
2020-01-25 12:26:41 -0500

    vertical scrolling implemented

--- muse.c
+++ muse.c
@@ -39,6 +39,7 @@ typedef struct erow {
 
 struct editorConfig {
     int cx, cy; // Cursor x and y position in characters
+    int rowoff;  // Offset of row displayed
     int screenrows; // Number of rows that we can show
     int screencols; // Number of cols that we can show
     int numrows; // Number of rows
@@ -239,12 +240,22 @@ void abFree(struct abuf *ab) {
 
 /*** output ***/
 
+void editorScroll() {
+    if (E.cy < E.rowoff) {
+        E.rowoff = E.cy;
+    }
+    if (E.cy >= E.rowoff + E.screenrows) {
+        E.rowoff = E.cy - E.screenrows + 1;
+    }
+}
+
 // Draw each row of the buffer of text being edited
 void editorDrawRows(struct abuf *ab) {
     int y;
 
     for (y = 0; y < E.screenrows; y++) {
-        if (y >= E.numrows) {
+        int filerow = y + E.rowoff;
+        if (filerow >= E.numrows) {
             // Write welcome message or ~
             if (E.numrows == 0 && y == E.screenrows / 3) {
                 char welcome[80];
@@ -264,9 +275,9 @@ void editorDrawRows(struct abuf *ab) {
                 abAppend(ab, "~", 1);
             }
         } else {
-            int len = E.row[y].size;
+            int len = E.row[filerow].size;
             if (len > E.screencols) len = E.screencols;
-            abAppend(ab, E.row[y].chars, len);
+            abAppend(ab, E.row[filerow].chars, len);
         }
 
         abAppend(ab, "\x1b[K", 3);
@@ -278,6 +289,8 @@ void editorDrawRows(struct abuf *ab) {
 
 // Clear the screen
 void editorRefreshScreen() {
+    editorScroll();
+
     struct abuf ab = ABUF_INIT;
 
     abAppend(&ab, "\x1b[?25l", 6); // Hide the cursor
@@ -287,7 +300,7 @@ void editorRefreshScreen() {
 
     // Move the cursor to the position stored in E.cx and E.cy
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
     abAppend(&ab, buf, strlen(buf));
 
     abAppend(&ab, "\x1b[?25h", 6); // Display the cusor
@@ -317,7 +330,7 @@ void editorMoveCursor(int key) {
             }
             break;
         case ARROW_DOWN:
-            if (E.cy != E.screenrows - 1) {
+            if (E.cy < E.numrows) {
                 E.cy++;
             }
             break;
@@ -365,6 +378,7 @@ void editorProcessKeypress() {
 void initEditor() {
     E.cx = 0; // Horizontal coordinate of the cursor (the column)
     E.cy = 0; // Vertical coordinate of the cursor (the row)
+    E.rowoff = 0;
     E.numrows = 0;
     E.row = NULL;
 
