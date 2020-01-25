3c7f232b15792e9685e9a68e3c1d1a7453936e87
Jeremy Boggs <nicocchilover@gmail.com>
2020-01-25 15:16:15 -0500

    search match syntax

--- muse.c
+++ muse.c
@@ -38,7 +38,8 @@ enum editorKey {
 
 enum editorHighlight {
     HL_NORMAL = 0,
-    HL_NUMBER
+    HL_NUMBER,
+    HL_MATCH
 };
 
 /*** data ***/
@@ -226,6 +227,7 @@ void editorUpdateSyntax(erow *row) {
 int editorSyntaxToColor(int hl) {
     switch(hl) {
         case HL_NUMBER: return 31;
+        case HL_MATCH: return 34;
         default: return 37;
     }
 }
@@ -510,6 +512,8 @@ void editorFindCallback(char *query, int key) {
             E.cy = current;
             E.cx = editorRowRXtoCX(row, match - row->render);
             E.rowoff = E.numrows;
+
+            memset(&row->hl[match -row->render], HL_MATCH, strlen(query));
             break;
         }
     }
