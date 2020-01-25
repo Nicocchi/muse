0b4d3432eaa217e8655933cdfe82027ea9b7aa78
Jeremy Boggs <nicocchilover@gmail.com>
2020-01-25 16:14:22 -0500

    updated number syntax to be only do numbers via esc keys and decimal points

--- muse.c
+++ muse.c
@@ -212,15 +212,26 @@ int getWindowSize(int *rows, int*cols) {
 
 /*** syntax highlighting ***/
 
+// Return true if it's a separator character
+int is_separator(int c) {
+    return isspace(c) || c == '\0' || strchr(",.()+-/*=~%<>[];", c) != NULL;
+}
+
 void editorUpdateSyntax(erow *row) {
     row->hl = realloc(row->hl, row->rsize);
     memset(row->hl, HL_NORMAL, row->rsize);
 
-    int i;
-    for (i = 0; i < row->rsize; i++) {
-        if (isdigit(row->render[i])) {
+    int prev_sep = 1;
+
+    int i = 0;
+    while (i < row->rsize) {
+        char c = row->render[i];
+        unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+        if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) || (c == '.' && prev_hl == HL_NUMBER)) {
             row->hl[i] = HL_NUMBER;
         }
+        i++;
     }
 }
 
